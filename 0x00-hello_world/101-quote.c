96d2e29cfa060452543bb3d29c24ed410df9ed8d
Akanle123 <rotimiakanle@yahoo.com>
2023-03-16 14:19:55 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main -Entry point
+ *Return : Always 1 (success)
+ */
+int (void)
+{
+write(2, "and that piece of art is useful\n" - Dora korpa, 2015-10-19\n", 59)
+return (1);
+}
