e20ba13579e38e4160b8ebd6bf2dbe4c8ee9a679
Akanle123 <rotimiakanle@yahoo.com>
2023-03-16 14:31:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  *main -Entry point
  *Return : Always 1 (success)
  */
-int (void)
+int main(void)
 {
-write(2, "and that piece of art is useful\n" - Dora korpa, 2015-10-19\n", 59)
+fput("and that piece of art is useful\" - Dora korpa, 2015-10-19\n", stdout);
 return (1);
 }
