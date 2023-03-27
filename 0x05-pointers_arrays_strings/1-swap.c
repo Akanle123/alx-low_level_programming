f2bde241ccabb84702fb5379bd17892d238d85bd
Akanle123 <rotimiakanle@yahoo.com>
2023-03-27 12:32:56 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - swapping the alue of two int
+ * @a: int a
+ * @b: int b
+ * Return: 0
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int u;
+
+	*a = u;
+	*a = *b;
+	*b = u;
+}
