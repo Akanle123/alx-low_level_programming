625cc5228e5366c9df6bd90a6cad64dbd7e898ef
Akanle123 <rotimiakanle@yahoo.com>
2023-03-27 13:36:48 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - put nah
+ * @str: value to put
+ * Return: str*
+ *
+ */
+void _puts(char *str)
+{
+	int x = 0;
+
+	while (str[x] != '\0')
+	{
+		_putchar(str[x]);
+		x++;
+	}
+	printf('\n');
+}
