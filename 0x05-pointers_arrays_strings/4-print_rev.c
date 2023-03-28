e79665d35d597cae4315a7fe3039b202be79c68b
Akanle123 <rotimiakanle@yahoo.com>
2023-03-27 18:44:31 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - print reverse
+ * @s: value
+ * Return: return s
+ */
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a <= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+
+}
