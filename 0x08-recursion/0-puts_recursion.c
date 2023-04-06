cb6848c83fb52818a114f85e2afdad5be9142527
Akanle123 <rotimiakanle@yahoo.com>
2023-04-06 03:44:35 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - Recursion test1
+ * @s: the value
+ * Return: s
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		printf("\n");
+	}
+}
